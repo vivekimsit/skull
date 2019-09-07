c661dcbf0f03ed1a1415122fb9e281100fa46899
Vivek Poddar <vivekimsit@gmail.com>
2019-09-07 16:38:41 +0530

    Init

---
+++ config.js
@@ -0,0 +1,38 @@
+module.exports = {
+	siteTitle: 'One Finess', // <title>
+	manifestName: 'SolidState',
+	manifestShortName: 'Landing', // max 12 characters
+	manifestStartUrl: '/',
+	manifestBackgroundColor: '#663399',
+	manifestThemeColor: '#663399',
+	manifestDisplay: 'standalone',
+	manifestIcon: 'src/assets/img/website-icon.png',
+	pathPrefix: `/gatsby-starter-solidstate/`, // This path is subpath of your hosting https://domain/portfolio
+	heading: 'Dinesh Sharma',
+	subHeading: 'Platinum trainer, body transformation expert.',
+	// social
+	socialLinks: [
+		{
+			icon: 'fa-github',
+			name: 'Github',
+			url: 'https://github.com/anubhavsrivastava',
+		},
+		{
+			icon: 'fa-twitter',
+			name: 'Twitter',
+			url: 'https://twitter.com/onlyanubhav',
+		},
+		{
+			icon: 'fa-facebook',
+			name: 'Facebook',
+			url: 'https://facebook.com/theanubhav',
+		},
+		{
+			icon: 'fa-envelope-o',
+			name: 'Email',
+			url: 'mailto:anubhav.srivastava00@gmail.com',
+		},
+	],
+	phone: '000-00000',
+	address: '305 Del Sol, Sunnyvale, CA',
+};
